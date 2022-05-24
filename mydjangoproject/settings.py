76a3b58ade08ac577509553c4c33d62b1005f99d
daithioc95 <51501850+daithioc95@users.noreply.github.com>
2022-05-24 17:13:18 +0100

    Update database settings

--- mydjangoproject/settings.py
+++ mydjangoproject/settings.py
@@ -105,8 +105,12 @@ WSGI_APPLICATION = 'mydjangoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
