6330a50154020aceb6a5bd74bdb0017644919447
daithioc95 <51501850+daithioc95@users.noreply.github.com>
2022-05-24 17:19:06 +0100

    Updated allowed hosts to all

--- mydjangoproject/settings.py
+++ mydjangoproject/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['react-django-weather-app.herokuapp.com']
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
