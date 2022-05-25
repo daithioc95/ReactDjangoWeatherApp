ac7ff392aeff91cf11784355e77ff65568adadd7
daithioc95 <51501850+daithioc95@users.noreply.github.com>
2022-05-25 16:21:58 +0100

    Update apisearchcall api link from LocationCard axios

--- mydjangoproject/settings.py
+++ mydjangoproject/settings.py
@@ -101,6 +101,7 @@ WSGI_APPLICATION = 'mydjangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
