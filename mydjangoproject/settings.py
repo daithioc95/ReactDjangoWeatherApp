0697f495f59e9cad06b7971b9d71f13d1e5eb50b
daithioc95 <51501850+daithioc95@users.noreply.github.com>
2022-05-25 11:41:09 +0100

    update congig secret key to heroku

--- mydjangoproject/settings.py
+++ mydjangoproject/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
 from decouple import config 
 import dj_database_url
@@ -28,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
