1c2e1dcebc0ee6f1c2ed01363e74f02ba0441959
daithioc95 <51501850+daithioc95@users.noreply.github.com>
2022-07-29 15:00:09 +0100

    Footer background updated to svg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import footerImage from '../assets/wave.svg';
 
 class Footer extends React.Component {
   render() {
     return (
       <footer>
+        <img alt="footer-waves" src={footerImage}/>
         <p className="copyright">Designed by&nbsp;
           <a href="https://daithioc95.github.io/MS1/" target="_blank" rel="noreferrer">
             CodeDoc
