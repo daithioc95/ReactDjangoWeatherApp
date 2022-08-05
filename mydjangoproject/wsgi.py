db55865a9e1c6fe1baf2d464838509e425742c4b
daithioc95 <51501850+daithioc95@users.noreply.github.com>
2022-08-05 12:17:24 +0100

    Check image files with updated wsgi settings with application referenced in wsgi.py

--- mydjangoproject/wsgi.py
+++ mydjangoproject/wsgi.py
@@ -15,5 +15,5 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mydjangoproject.settings')
 
-# application = get_wsgi_application()
+application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'build'))
