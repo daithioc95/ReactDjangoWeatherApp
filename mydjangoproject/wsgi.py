f0700d9f515e7f2bd568ae07c03ff7607a2eff20
daithioc95 <51501850+daithioc95@users.noreply.github.com>
2022-08-05 12:11:38 +0100

    Check image files with updated wsgi settings

--- mydjangoproject/wsgi.py
+++ mydjangoproject/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mydjangoproject.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'build'))
