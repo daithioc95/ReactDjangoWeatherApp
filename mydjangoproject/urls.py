53dcdb2940ddaaadee160989dda1fb23ebc9f8ab
daithioc95 <51501850+daithioc95@users.noreply.github.com>
2022-08-25 21:19:26 +0100

    Undo catch all changes

--- mydjangoproject/urls.py
+++ mydjangoproject/urls.py
@@ -19,8 +19,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^$', include('mainApp.urls')),
-    path(r'^$', include('favourites.urls')),
+    path('', include('mainApp.urls')),
+    path('', include('favourites.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
     path('accounts/', include('allauth.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
