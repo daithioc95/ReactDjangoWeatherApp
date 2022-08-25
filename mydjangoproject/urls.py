8fc605618e4abc8d3e13ad007f2cd08ff024632b
daithioc95 <51501850+daithioc95@users.noreply.github.com>
2022-08-25 20:27:20 +0100

    Implement catch all approach for 404 server error

--- mydjangoproject/urls.py
+++ mydjangoproject/urls.py
@@ -19,9 +19,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainApp.urls')),
-    path('Favourites', include('favourites.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    # path('', include('mainApp.urls')),
+    # path('', include('mainApp.urls')),
+    # path('', include('favourites.urls')),
+    path(r'^$', TemplateView.as_view(template_name='index.html')),
     path('accounts/', include('allauth.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
