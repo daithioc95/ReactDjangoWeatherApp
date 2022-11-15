84ca77c6f4b3413842db4105d410b5084a5ff715
daithioc95 <51501850+daithioc95@users.noreply.github.com>
2022-11-15 15:23:42 +0000

    Revert previous line removal

--- mydjangoproject/urls.py
+++ mydjangoproject/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('', include('mainApp.urls')),
     path('', include('favourites.urls')),
